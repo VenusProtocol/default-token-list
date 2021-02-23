0d2c4928939c66b76aa9902d267aee971d8a9887
gryincent <grygoriy@swipe.io>
2021-02-23 21:29:44 +1000

    add venus default token list

---
+++ .github/ISSUE_TEMPLATE/token-request.md
@@ -0,0 +1,22 @@
+---
+name: Token Request
+about: Request a token addition
+title: 'Add {TOKEN_SYMBOL}: {TOKEN_NAME}'
+labels: token request
+assignees: ''
+---
+
+- [ ] I understand that token listing is not required to use the Venus Interface with a token.
+- [ ] I understand that filing an issue or adding liquidity does not guarantee addition to the Venus default token list.
+- [ ] I will not ping the Discord about this listing request.
+
+**Please provide the following information for your token.**
+
+Token Address: 
+Token Name (from contract): 
+Token Decimals (from contract): 
+Token Symbol (from contract): 
+Venus Address of vToken: 
+
+Link to the official homepage of token:
+Link to CoinMarketCap or CoinGecko page of token: 
---
+++ src/buildList.js
@@ -0,0 +1,26 @@
+const { version } = require("../package.json");
+const mainnet = require("./tokens/mainnet.json");
+
+module.exports = function buildList() {
+  const parsed = version.split(".");
+  return {
+    name: "Venus Default List",
+    timestamp: new Date().toISOString(),
+    version: {
+      major: +parsed[0],
+      minor: +parsed[1],
+      patch: +parsed[2],
+    },
+    tags: {},
+    logoURI: "https://venus.io/images/markets/XVS.png",
+    keywords: ["venus", "default", "bsc"],
+    tokens: [...mainnet]
+      // sort them by symbol for easy readability
+      .sort((t1, t2) => {
+        if (t1.chainId === t2.chainId) {
+          return t1.symbol.toLowerCase() < t2.symbol.toLowerCase() ? -1 : 1;
+        }
+        return t1.chainId < t2.chainId ? -1 : 1;
+      }),
+  };
+};
